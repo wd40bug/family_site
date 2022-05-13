984a91a971a283b1c7ecb790c5dbd03f8aa7b942
wd40bug <wd40bug@gmail.com>
2022-05-12 18:11:40 -0600

    will work?

--- like_button.js
+++ like_button.js
@@ -3,21 +3,24 @@
 const e = React.createElement;
 
 class LikeButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { liked: false };
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
     }
 
-    render() {
-        if (this.state.liked) {
-            return 'You liked this.';
-        }
-
-        return e(
-            'button',
-            { onClick: () => this.setState({ liked: true }) },
-            'Like'
-        );
-    }
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
 }
-const domContainer = document.querySelector('#like_button_container'); const root = ReactDOM.createRoot(domContainer); root.render(e(LikeButton));
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
