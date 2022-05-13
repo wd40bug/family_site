dac97a0d1418a6bbae1b666b3fe4f9fb1d2c6d75
wd40bug <wd40bug@gmail.com>
2022-05-13 07:18:19 -0600

    changed the word on the button

--- scripts/like_button.js
+++ scripts/like_button.js
@@ -10,13 +10,13 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return 'You liked this!';
+      return 'You clicked me!';
     }
 
     return e(
       'button',
       { onClick: () => this.setState({ liked: true }) },
-      'Like'
+      'Im a react element!'
     );
   }
 }
